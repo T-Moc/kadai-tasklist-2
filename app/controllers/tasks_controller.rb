349bfe150c434ff7f6721233f6ecb7dda4bd3504
momoto.saito <otoja.like@gmail.com>
2021-03-04 13:13:50 +0000

    refix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController 
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy, :update]
   before_action :set_task, only:[:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :destroy, :update]
+ 
   
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])

6377c810f81ecceb5db121cfe270d2fe20e2f8c1
momoto.saito <otoja.like@gmail.com>
2021-03-04 14:09:20 +0000

    fix threetimes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController 
   before_action :require_user_logged_in
-  before_action :set_task, only:[:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:show, :edit, :destroy, :update]
  
   
@@ -24,7 +23,7 @@ class TasksController < ApplicationController
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Taskの投稿に失敗しました。'
-      render 'tasks/index'
+      render :new
     end
   end
 
@@ -48,9 +47,7 @@ class TasksController < ApplicationController
   end
 
   private
-  def set_task
-    @task=Task.find(params[:id])
-  end
+  
 
   def task_params
     params.require(:task).permit(:content, :status)
